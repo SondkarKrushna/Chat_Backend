fc7378385b647f7e1b2c6c4acdf38ddc0f5dd111
Krushna Sondkar <sondkarkrushna@gmail.com>
2026-02-27 15:16:55 +0530

    roled back

--- socket/socketHandler.js
+++ socket/socketHandler.js
@@ -19,7 +19,7 @@ const socketHandler = (io) => {
   io.on("connection", (socket) => {
     console.log("User connected:", socket.user);
 
-    // ✅ FIXED HERE
+    // 🔥 FIX: Safely extract correct userId
     const userId = socket.user.id;
 
     if (!userId) {
@@ -27,20 +27,31 @@ const socketHandler = (io) => {
       return;
     }
 
-    socket.join(userId.toString()); // safer
+    // console.log("✅ Joining room:", userId);
+
+    socket.join(userId);
 
     socket.on("sendMessage", async ({ receiverId, message }) => {
+      console.log("🔥 Server received message:", {
+        sender: socket.user,
+        receiverId,
+        message
+      });
+      console.log("📨 Message received:", { receiverId, message });
+
       const newMessage = await Message.create({
         sender: userId,
         receiver: receiverId,
         message,
       });
 
+      console.log("Joining room:", socket.user._id);
+
       // Send to receiver
-      io.to(receiverId.toString()).emit("receiveMessage", newMessage);
+      io.to(receiverId).emit("receiveMessage", newMessage);
 
       // Send back to sender
-      io.to(userId.toString()).emit("receiveMessage", newMessage);
+      io.to(userId).emit("receiveMessage", newMessage);
     });
 
     socket.on("disconnect", () => {
